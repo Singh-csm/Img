1f6edcd7340211123d47c916434424f1e3057174
Singh-csm <csm8107@gmail.com>
2023-06-07 13:34:49 +0530

    iMg: play

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{display:"flex", justifyContent:"space-between"}}>
+      <div style={{border:"5px solid orange", borderRadius:"50%", blockSize:"100px",boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 10, 0, 12) 0px 12px 30px, rgba(0, 10, 0, 12) 0px 4px 6px, rgba(0, 0, 0, 17) 0px 12px 13px, rgba(0, 10, 0, 19) 0px 3px 15px"}}>
+      <img style={{blockSize:"100px",  borderRadius:"50%"}} src="image1.png" alt='img' />
+      </div>
+      <div style={{border:"5px solid orange", borderRadius:"50%", blockSize:"100px", boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 10, 0, 12) 0px 12px 30px, rgba(0, 10, 0, 12) 0px 4px 6px, rgba(0, 0, 0, 17) 0px 12px 13px, rgba(0, 10, 0, 19) 0px 3px 15px"}}>
+      <img style={{blockSize:"100px",  borderRadius:"50%"}} src="image1.png" alt='img' />
+      </div>  
+      <div style={{border:"5px solid orange", borderRadius:"50%", blockSize:"100px", boxShadow: "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 10, 0, 12) 0px 12px 30px, rgba(0, 10, 0, 12) 0px 4px 6px, rgba(0, 0, 0, 17) 0px 12px 13px, rgba(0, 10, 0, 19) 0px 3px 15px"}}>
+      <img style={{blockSize:"100px", borderRadius:"50%"}} src="image1.png" alt='img' />
+      </div>
+    </div>
+  )
+}
+
+export default App
